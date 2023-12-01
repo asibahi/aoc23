1c89b82ddf1456d4e3c57758b5fd25e95ba52e9a
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-12-01 22:59:57 +0300

    day2 templetate

---
+++ src/bin/day2.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let input = include_str!("../input/day1.txt");
+
+    let res = solve_1(input);
+    println!("Part 1:\t{res}");
+
+    let res = solve_2(input);
+    println!("Part 2:\t{res}");
+}
+
+fn solve_1(i: &str) -> u32 {
+    0
+}
+
+fn solve_2(i: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("two1nine", 29)]
+    fn test(i: &str, r: u32) {
+        let actual = solve_1(i);
+        assert_eq!(actual, r)
+    }
+}
