5458d670f73f7b6b9ce64590fb3ae669b9270813
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-12-07 12:39:44 +0300

    day 7 part 2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -2,103 +2,195 @@
 
 use std::collections::BTreeMap;
 
-use counter::Counter;
-use itertools::Itertools;
-
 const INPUT: &str = include_str!("../../input/day7.txt");
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
-enum Rank {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace,
-}
+mod part_1 {
+    use counter::Counter;
+    use itertools::Itertools;
 
-impl From<u8> for Rank {
-    fn from(value: u8) -> Self {
-        match value {
-            b'A' => Self::Ace,
-            b'K' => Self::King,
-            b'Q' => Self::Queen,
-            b'J' => Self::Jack,
-            b'T' => Self::Ten,
-            b'9' => Self::Nine,
-            b'8' => Self::Eight,
-            b'7' => Self::Seven,
-            b'6' => Self::Six,
-            b'5' => Self::Five,
-            b'4' => Self::Four,
-            b'3' => Self::Three,
-            _ => Self::Two,
+    #[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
+    enum Rank {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
+    }
+
+    impl From<u8> for Rank {
+        fn from(value: u8) -> Self {
+            match value {
+                b'A' => Self::Ace,
+                b'K' => Self::King,
+                b'Q' => Self::Queen,
+                b'J' => Self::Jack,
+                b'T' => Self::Ten,
+                b'9' => Self::Nine,
+                b'8' => Self::Eight,
+                b'7' => Self::Seven,
+                b'6' => Self::Six,
+                b'5' => Self::Five,
+                b'4' => Self::Four,
+                b'3' => Self::Three,
+                _ => Self::Two,
+            }
+        }
+    }
+
+    #[derive(PartialEq, Eq, PartialOrd, Ord)]
+    enum HandType {
+        HighCard,
+        OnePair,
+        TwoPair,
+        ThreeOfAKind,
+        FullHouse,
+        FourOfAKind,
+        FiveOfAKind,
+    }
+
+    #[derive(PartialEq, Eq, PartialOrd, Ord)]
+    pub(crate) struct Hand {
+        hand_type: HandType,
+        literal: (Rank, Rank, Rank, Rank, Rank),
+    }
+
+    impl Hand {
+        pub fn parse(input: &str) -> Self {
+            // for example: 32T3K
+
+            let counts = input.bytes().collect::<Counter<_>>();
+            let counts = counts.values().sorted().rev().collect_vec();
+            // let counts = counts.most_common();
+
+            let hand_type = if *counts[0] == 5 {
+                HandType::FiveOfAKind
+            } else if *counts[0] == 4 {
+                HandType::FourOfAKind
+            } else if *counts[0] == 3 && *counts[1] == 2 {
+                HandType::FullHouse
+            } else if *counts[0] == 3 {
+                HandType::ThreeOfAKind
+            } else if *counts[0] == 2 && *counts[1] == 2 {
+                HandType::TwoPair
+            } else if *counts[0] == 2 {
+                HandType::OnePair
+            } else {
+                HandType::HighCard
+            };
+
+            let literal = input
+                .bytes()
+                .map(|b| Rank::from(b))
+                .collect_tuple()
+                .unwrap();
+
+            Self { hand_type, literal }
         }
     }
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-enum HandType {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    FullHouse,
-    FourOfAKind,
-    FiveOfAKind,
-}
+mod part_2 {
+    use counter::Counter;
+    use itertools::Itertools;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-struct Hand {
-    hand_type: HandType,
-    literal: (Rank, Rank, Rank, Rank, Rank),
-}
+    #[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
+    enum Rank {
+        Joker,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Queen,
+        King,
+        Ace,
+    }
 
-impl Hand {
-    fn parse(input: &str) -> Self {
-        // for example: 32T3K
+    impl From<u8> for Rank {
+        fn from(value: u8) -> Self {
+            match value {
+                b'A' => Self::Ace,
+                b'K' => Self::King,
+                b'Q' => Self::Queen,
+                b'J' => Self::Joker,
+                b'T' => Self::Ten,
+                b'9' => Self::Nine,
+                b'8' => Self::Eight,
+                b'7' => Self::Seven,
+                b'6' => Self::Six,
+                b'5' => Self::Five,
+                b'4' => Self::Four,
+                b'3' => Self::Three,
+                _ => Self::Two,
+            }
+        }
+    }
 
-        let counts = input.bytes().map(|b| Rank::from(b)).collect::<Counter<_>>();
-        // .fold(BTreeMap::new(), |mut map, card| {
-        //     *map.entry(card).or_default() += 1;
-        //     map
-        // });
+    #[derive(PartialEq, Eq, PartialOrd, Ord)]
+    enum HandType {
+        HighCard,
+        OnePair,
+        TwoPair,
+        ThreeOfAKind,
+        FullHouse,
+        FourOfAKind,
+        FiveOfAKind,
+    }
 
-        let counts = counts.values().sorted().rev().collect_vec();
+    #[derive(PartialEq, Eq, PartialOrd, Ord)]
+    pub(crate) struct Hand {
+        hand_type: HandType,
+        literal: (Rank, Rank, Rank, Rank, Rank),
+    }
 
-        let hand_type = if *counts[0] == 5 {
-            HandType::FiveOfAKind
-        } else if *counts[0] == 4 {
-            HandType::FourOfAKind
-        } else if *counts[0] == 3 && *counts[1] == 2 {
-            HandType::FullHouse
-        } else if *counts[0] == 3 {
-            HandType::ThreeOfAKind
-        } else if *counts[0] == 2 && *counts[1] == 2 {
-            HandType::TwoPair
-        } else if *counts[0] == 2 {
-            HandType::OnePair
-        } else {
-            HandType::HighCard
-        };
+    impl Hand {
+        pub fn parse(input: &str) -> Self {
+            // for example: 32T3K
 
-        let literal = input
-            .bytes()
-            // .inspect(|b| {
-            //     dbg!(*b as char);
-            // })
-            .map(|b| Rank::from(b))
-            .collect_tuple()
-            .unwrap();
+            let mut counts = input.bytes().collect::<Counter<_>>();
+            let jokers = counts.remove(&b'J').unwrap_or_default();
+            let counts = counts.values().sorted().rev().collect_vec();
 
-        Self { hand_type, literal }
+            let hand_type = if counts.is_empty() || *counts[0] + jokers == 5 {
+                HandType::FiveOfAKind
+            } else if *counts[0] + jokers == 4 {
+                HandType::FourOfAKind
+            } else if *counts[0] + jokers == 3 && *counts[1] == 2 {
+                HandType::FullHouse
+            } else if *counts[0] == 3 && *counts[1] + jokers == 2 {
+                HandType::FullHouse
+            } else if *counts[0] + jokers == 3 {
+                HandType::ThreeOfAKind
+            } else if *counts[0] + jokers == 2 && *counts[1] == 2 {
+                HandType::TwoPair
+            } else if *counts[0] == 2 && *counts[1] + jokers == 2 {
+                HandType::TwoPair
+            } else if *counts[0] + jokers == 2 {
+                HandType::OnePair
+            } else {
+                HandType::HighCard
+            };
+
+            let literal = input
+                .bytes()
+                .map(|b| Rank::from(b))
+                .collect_tuple()
+                .unwrap();
+
+            Self { hand_type, literal }
+        }
     }
 }
 
@@ -116,7 +208,7 @@ fn solve_1(input: &str) -> usize {
     for line in input.lines() {
         let (hand, bid) = line.split_at(5);
         let bid = bid.trim().parse::<usize>().unwrap();
-        let hand = Hand::parse(hand);
+        let hand = part_1::Hand::parse(hand);
 
         map.insert(hand, bid);
     }
@@ -125,7 +217,17 @@ fn solve_1(input: &str) -> usize {
 }
 
 fn solve_2(input: &str) -> usize {
-    input.lines().map(|i| i.len()).sum()
+    let mut map = BTreeMap::new();
+
+    for line in input.lines() {
+        let (hand, bid) = line.split_at(5);
+        let bid = bid.trim().parse::<usize>().unwrap();
+        let hand = part_2::Hand::parse(hand);
+
+        map.insert(hand, bid);
+    }
+
+    map.values().enumerate().map(|(i, bid)| (i + 1) * bid).sum()
 }
 
 #[cfg(test)]
@@ -140,7 +242,12 @@ mod tests {
         solve_1(i)
     }
 
-    /* #[test]
+    #[test_case(EXAMPLE => 5905)]
+    fn test_part_2(i: &str) -> usize {
+        solve_2(i)
+    }
+
+    #[test]
     fn bench() {
         use microbench::{self, Options};
 
@@ -150,5 +257,5 @@ mod tests {
         let options = Options::default();
         microbench::bench(&options, "original part 1", || solve_1(INPUT));
         microbench::bench(&options, "original part 2", || solve_2(INPUT));
-    } */
+    }
 }
