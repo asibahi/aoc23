24b4448ec8c0d7eed97ce301ef52d0390b47ad78
Abdul Rahman Sibahi <asibahi@users.noreply.github.com>
2023-12-10 18:06:39 +0300

    better part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -8,8 +8,8 @@ fn main() {
     println!("Part 2:\t{res}");
 }
 
-#[derive(Debug)]
-enum Direction {
+#[derive(Debug, Clone, Copy)]
+enum Dir {
     Up,
     Right,
     Down,
@@ -17,7 +17,7 @@ enum Direction {
 }
 
 fn solve_1(input: &str) -> usize {
-    let width = input.lines().next_back().unwrap().len() + 1;
+    let width = input.lines().next().unwrap().len() + 1;
 
     let mut current_loc = {
         let start_loc = input.find('S').unwrap();
@@ -40,17 +40,17 @@ fn solve_1(input: &str) -> usize {
             if new_pipe == b'7' || new_pipe == b'F' || new_pipe == b'|' {
                 current_loc = (row, col);
                 current_pipe = new_pipe;
-                came_by = Some(Direction::Up);
+                came_by = Some(Dir::Up);
                 counter += 1;
                 continue;
             }
             // check right
             let (row, col) = (current_loc.0, current_loc.1 + 1);
             let new_pipe = input.as_bytes()[row * width + col];
-            if new_pipe == b'7' || new_pipe == b'J' || new_pipe == b'_' {
+            if new_pipe == b'7' || new_pipe == b'J' || new_pipe == b'-' {
                 current_loc = (row, col);
                 current_pipe = new_pipe;
-                came_by = Some(Direction::Right);
+                came_by = Some(Dir::Right);
                 counter += 1;
                 continue;
             }
@@ -60,17 +60,17 @@ fn solve_1(input: &str) -> usize {
             if new_pipe == b'J' || new_pipe == b'L' || new_pipe == b'|' {
                 current_loc = (row, col);
                 current_pipe = new_pipe;
-                came_by = Some(Direction::Down);
+                came_by = Some(Dir::Down);
                 counter += 1;
                 continue;
             }
             // check left
             let (row, col) = (current_loc.0, current_loc.1 - 1); // OVERFLOW
             let new_pipe = input.as_bytes()[row * width + col];
-            if new_pipe == b'L' || new_pipe == b'F' || new_pipe == b'_' {
+            if new_pipe == b'L' || new_pipe == b'F' || new_pipe == b'-' {
                 current_loc = (row, col);
                 current_pipe = new_pipe;
-                came_by = Some(Direction::Left);
+                came_by = Some(Dir::Left);
                 counter += 1;
                 continue;
             }
@@ -80,32 +80,32 @@ fn solve_1(input: &str) -> usize {
         counter += 1;
         match (dir, current_pipe) {
             // go right
-            (Direction::Up, b'F') | (Direction::Down, b'L') | (Direction::Right, b'-') => {
+            (Dir::Up, b'F') | (Dir::Down, b'L') | (Dir::Right, b'-') => {
                 let (row, col) = (current_loc.0, current_loc.1 + 1);
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Right);
+                came_by = Some(Dir::Right);
             }
             // go up
-            (Direction::Right, b'J') | (Direction::Left, b'L') | (Direction::Up, b'|') => {
+            (Dir::Right, b'J') | (Dir::Left, b'L') | (Dir::Up, b'|') => {
                 let (row, col) = (current_loc.0 - 1, current_loc.1);
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Up);
+                came_by = Some(Dir::Up);
             }
             // go down
-            (Direction::Right, b'7') | (Direction::Left, b'F') | (Direction::Down, b'|') => {
+            (Dir::Right, b'7') | (Dir::Left, b'F') | (Dir::Down, b'|') => {
                 let (row, col) = (current_loc.0 + 1, current_loc.1); // OVERFLOW
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Down);
+                came_by = Some(Dir::Down);
             }
             // go left
-            (Direction::Down, b'J') | (Direction::Up, b'7') | (Direction::Left, b'-') => {
+            (Dir::Down, b'J') | (Dir::Up, b'7') | (Dir::Left, b'-') => {
                 let (row, col) = (current_loc.0, current_loc.1 - 1); // OVERFLOW
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Left);
+                came_by = Some(Dir::Left);
             }
             (_, b'S') => {
                 break;
@@ -118,7 +118,7 @@ fn solve_1(input: &str) -> usize {
 }
 
 fn solve_2(input: &str) -> usize {
-    let width = input.lines().next_back().unwrap().len() + 1;
+    let width = input.lines().next().unwrap().len() + 1;
 
     let mut current_loc = {
         let start_loc = input.find('S').unwrap();
@@ -143,7 +143,7 @@ fn solve_2(input: &str) -> usize {
                 if new_pipe == b'7' || new_pipe == b'F' || new_pipe == b'|' {
                     current_loc = (row, col);
                     current_pipe = new_pipe;
-                    came_by = Some(Direction::Up);
+                    came_by = Some(Dir::Up);
                     points.push((row, col));
                     continue;
                 }
@@ -151,10 +151,10 @@ fn solve_2(input: &str) -> usize {
             // check right
             let (row, col) = (current_loc.0, current_loc.1 + 1);
             let new_pipe = input.as_bytes()[row * width + col];
-            if new_pipe == b'7' || new_pipe == b'J' || new_pipe == b'_' {
+            if new_pipe == b'7' || new_pipe == b'J' || new_pipe == b'-' {
                 current_loc = (row, col);
                 current_pipe = new_pipe;
-                came_by = Some(Direction::Right);
+                came_by = Some(Dir::Right);
                 points.push((row, col));
                 continue;
             }
@@ -164,17 +164,17 @@ fn solve_2(input: &str) -> usize {
             if new_pipe == b'J' || new_pipe == b'L' || new_pipe == b'|' {
                 current_loc = (row, col);
                 current_pipe = new_pipe;
-                came_by = Some(Direction::Down);
+                came_by = Some(Dir::Down);
                 points.push((row, col));
                 continue;
             }
             // check left
             let (row, col) = (current_loc.0, current_loc.1 - 1); // OVERFLOW
             let new_pipe = input.as_bytes()[row * width + col];
-            if new_pipe == b'L' || new_pipe == b'F' || new_pipe == b'_' {
+            if new_pipe == b'L' || new_pipe == b'F' || new_pipe == b'-' {
                 current_loc = (row, col);
                 current_pipe = new_pipe;
-                came_by = Some(Direction::Left);
+                came_by = Some(Dir::Left);
                 points.push((row, col));
                 continue;
             }
@@ -183,35 +183,35 @@ fn solve_2(input: &str) -> usize {
 
         match (dir, current_pipe) {
             // go right
-            (Direction::Up, b'F') | (Direction::Down, b'L') | (Direction::Right, b'-') => {
+            (Dir::Up, b'F') | (Dir::Down, b'L') | (Dir::Right, b'-') => {
                 let (row, col) = (current_loc.0, current_loc.1 + 1);
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Right);
+                came_by = Some(Dir::Right);
                 points.push((row, col));
             }
             // go up
-            (Direction::Right, b'J') | (Direction::Left, b'L') | (Direction::Up, b'|') => {
+            (Dir::Right, b'J') | (Dir::Left, b'L') | (Dir::Up, b'|') => {
                 let (row, col) = (current_loc.0 - 1, current_loc.1);
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Up);
+                came_by = Some(Dir::Up);
                 points.push((row, col));
             }
             // go down
-            (Direction::Right, b'7') | (Direction::Left, b'F') | (Direction::Down, b'|') => {
+            (Dir::Right, b'7') | (Dir::Left, b'F') | (Dir::Down, b'|') => {
                 let (row, col) = (current_loc.0 + 1, current_loc.1); // OVERFLOW
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Down);
+                came_by = Some(Dir::Down);
                 points.push((row, col));
             }
             // go left
-            (Direction::Down, b'J') | (Direction::Up, b'7') | (Direction::Left, b'-') => {
+            (Dir::Down, b'J') | (Dir::Up, b'7') | (Dir::Left, b'-') => {
                 let (row, col) = (current_loc.0, current_loc.1 - 1); // OVERFLOW
                 current_loc = (row, col);
                 current_pipe = input.as_bytes()[row * width + col];
-                came_by = Some(Direction::Left);
+                came_by = Some(Dir::Left);
                 points.push((row, col));
             }
             (_, b'S') => {
@@ -264,11 +264,171 @@ fn solve_2(input: &str) -> usize {
             let loc = (i / width, i % width);
             !points.contains(&loc) && count_intersections(loc.0, loc.1)
         })
-        // .inspect(|(i, c)| {
-        //     let loc = (i / width, i % width);
-        //     println!("{loc:?} , {}", *c as char)
-        // })
         .count()
+}
+
+#[allow(dead_code)]
+fn solve_2_try_2(input: &str) -> usize {
+    let width = input.lines().next().unwrap().len() + 1;
+
+    let mut current_loc = {
+        let start_loc = input.find('S').unwrap();
+        (
+            start_loc / width, // row
+            start_loc % width, // column
+        )
+    };
+    let input = input.as_bytes();
+
+    #[allow(unused_assignments)]
+    let mut true_nature_of_s = None;
+    let mut starting_direction = None;
+
+    let mut points = vec![];
+
+    let mut current_pipe = input[current_loc.0 * width + current_loc.1];
+    let mut came_by = None;
+
+    loop {
+        // This runs once
+        let Some(dir) = came_by.as_ref() else {
+            let (row, col, new_pipe) = 'checker: {
+                // check right
+                let (row, col) = (current_loc.0, current_loc.1 + 1);
+                let new_pipe = input[row * width + col];
+                if new_pipe == b'7' || new_pipe == b'J' || new_pipe == b'-' {
+                    came_by = Some(Dir::Right);
+                    break 'checker (row, col, new_pipe);
+                }
+                // check down
+                let (row, col) = (current_loc.0 + 1, current_loc.1);
+                let new_pipe = input[row * width + col];
+                if new_pipe == b'J' || new_pipe == b'L' || new_pipe == b'|' {
+                    came_by = Some(Dir::Down);
+                    break 'checker (row, col, new_pipe);
+                }
+                // check left
+                let (row, col) = (current_loc.0, current_loc.1 - 1);
+                let new_pipe = input[row * width + col];
+                if new_pipe == b'L' || new_pipe == b'F' || new_pipe == b'-' {
+                    came_by = Some(Dir::Left);
+                    break 'checker (row, col, new_pipe);
+                }
+                // check up
+                let (row, col) = (current_loc.0 - 1, current_loc.1);
+                let new_pipe = input[row * width + col];
+                if new_pipe == b'7' || new_pipe == b'F' || new_pipe == b'|' {
+                    came_by = Some(Dir::Up);
+                    break 'checker (row, col, new_pipe);
+                }
+                unreachable!()
+            };
+            starting_direction = came_by;
+            current_loc = (row, col);
+            current_pipe = new_pipe;
+            points.push((row, col));
+
+            continue;
+        };
+
+        let (row, col) = match (dir, current_pipe) {
+            // go right
+            (Dir::Up, b'F') | (Dir::Down, b'L') | (Dir::Right, b'-') => {
+                came_by = Some(Dir::Right);
+                (current_loc.0, current_loc.1 + 1)
+            }
+            // go up
+            (Dir::Right, b'J') | (Dir::Left, b'L') | (Dir::Up, b'|') => {
+                came_by = Some(Dir::Up);
+                (current_loc.0 - 1, current_loc.1)
+            }
+            // go down
+            (Dir::Right, b'7') | (Dir::Left, b'F') | (Dir::Down, b'|') => {
+                came_by = Some(Dir::Down);
+                (current_loc.0 + 1, current_loc.1)
+            }
+            // go left
+            (Dir::Down, b'J') | (Dir::Up, b'7') | (Dir::Left, b'-') => {
+                came_by = Some(Dir::Left);
+                (current_loc.0, current_loc.1 - 1)
+            }
+            (_, b'S') => {
+                let pipe = match (starting_direction.unwrap(), came_by.unwrap()) {
+                    (Dir::Down, Dir::Left) => b'F',
+                    (Dir::Down, Dir::Right) => b'7',
+                    (Dir::Up, Dir::Right) => b'J',
+                    (Dir::Up, Dir::Left) => b'L',
+                    (Dir::Up, Dir::Up) | (Dir::Down, Dir::Down) => b'|',
+                    (Dir::Left, Dir::Left) | (Dir::Right, Dir::Right) => b'-',
+                    (Dir::Right, Dir::Up) => b'F',
+
+                    // in testing
+                    (Dir::Left, Dir::Down) => b'J',
+                    (Dir::Right, Dir::Down) => b'L',
+                    (Dir::Left, Dir::Up) => b'7',
+
+                    // rest to be debugged. maybe unreachable
+                    (Dir::Up, Dir::Down) | (Dir::Down, Dir::Up) => b'|',
+                    (Dir::Left, Dir::Right) | (Dir::Right, Dir::Left) => b'-',
+                };
+
+                true_nature_of_s = Some(pipe);
+                break;
+            }
+            _ => unreachable!(),
+        };
+        current_loc = (row, col);
+        current_pipe = input[row * width + col];
+        points.push((row, col));
+    }
+
+    let mut inner_counter = 0;
+    let mut currently_inside = false;
+    let mut last_corner = None;
+
+    for (i, c) in input.into_iter().enumerate() {
+        let c = if *c == b'S' {
+            true_nature_of_s.unwrap()
+        } else {
+            *c
+        };
+        let loc = (i / width, i % width);
+        if points.contains(&loc) {
+            match c {
+                b'|' => {
+                    // print!("┃");
+                    currently_inside = !currently_inside
+                }
+                b'F' => {
+                    // print!("┏");
+                    last_corner = Some(b'F')
+                }
+                b'L' => {
+                    // print!("┗");
+                    last_corner = Some(b'L')
+                }
+                b'J' if last_corner.is_some_and(|c| c == b'F') => {
+                    // print!("┛");
+                    currently_inside = !currently_inside;
+                }
+                b'7' if last_corner.is_some_and(|c| c == b'L') => {
+                    // print!("┓");
+                    currently_inside = !currently_inside
+                }
+                // b'-' => print!("━"),
+                // b'J' => print!("┛"),
+                // b'7' => print!("┓"),
+                _ => {}
+            }
+        } else if currently_inside {
+            // print!("█");
+            inner_counter += 1;
+        } else {
+            // print!("{}", c as char);
+        }
+    }
+
+    inner_counter
 }
 
 #[cfg(test)]
@@ -295,18 +455,19 @@ mod tests {
     #[test_case(EXAMPLE5 => 8)]
     #[test_case(EXAMPLE6 => 10)]
     fn test_part_2(i: &str) -> usize {
-        solve_2(i)
+        solve_2_try_2(i)
     }
 
-    // #[test]
-    // fn bench() {
-    //     use microbench::{self, Options};
+    #[test]
+    fn bench() {
+        use microbench::{self, Options};
 
-    //     // use this terminal command
-    //     // cargo test --package aoc23 --bin day2 --release  -- tests::bench --exact --nocapture
+        // use this terminal command
+        // cargo test --package aoc23 --bin day10 --release  -- tests::bench --exact --nocapture
 
-    //     let options = Options::default();
-    //     microbench::bench(&options, "original part 1", || solve_1(INPUT));
-    //     microbench::bench(&options, "original part 2", || solve_2(INPUT));
-    // }
+        let options = Options::default();
+        microbench::bench(&options, "original part 1", || solve_1(INPUT));
+        microbench::bench(&options, "original part 2", || solve_2(INPUT));
+        microbench::bench(&options, "try 2    part 2", || solve_2_try_2(INPUT));
+    }
 }
